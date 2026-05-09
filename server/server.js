4731de5fd9a9043cf0e3f80ededa9e6cccbf84e9
Shariq Hussain <shariqhussain361@gmail.com>
2026-05-09 13:11:07 +0530

    connected to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.DB_URL).then((result)=>{
+    console.log("Connected to DB");
+}).catch((err)=>{
+    console.log("Error connecting to DB", err);
+});
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
