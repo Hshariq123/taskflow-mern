b6af30c5fa2db51da83cc6bc19441b60da0afb9a
Shariq Hussain <shariqhussain361@gmail.com>
2026-05-10 21:14:19 +0530

    added navigation bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
-function Navbar() {
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+function Navbar(active) {
   return (
-    <div>Navbar</div>
-    
+    <header>
+    <nav>
+         <div className='logo__wrapper'>
+         <img src={logo} alt="logo"/>
+         <h4>DoDo</h4>
+         </div>
+      <ul className="navigation-menu">
+             
+      <li><Link to="/" className={active==='home' && 'activeNav'}>Home</Link></li>
+
+        <li><Link to="/login">Login</Link></li>
+      <li><Link to="/register">Register</Link></li>
+      
+      
+        
+      </ul>
+    </nav>
+  </header>
   )
 }
 
