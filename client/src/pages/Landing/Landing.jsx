e99a1f6d75cdc5b0afb4790e9d20e1b3f1f51d33
Shariq Hussain <shariqhussain361@gmail.com>
2026-05-10 20:50:02 +0530

    added assets and css file

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
+import {Link} from 'react-router-dom'
+import landing from '../../assets/landing.jpg'
+import styles from './Landing.module.css'
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div>
+      <Navbar/>
+      <div className={styles.landing__wrapper}>
+        <h1>Schedule Your Daily Tasks With<span class="primaryText">DoDO!</span></h1>
+        <div className='btnWrapper'>
+          <Link to="/register" className = "primaryBtn">Register</Link>
+          <Link to="/login" className = "secondaryBtn">Login</Link>
+        </div>
+      </div>
+      <div className={styles.landing__img}>
+        <img src={landing} alt="landing"  />
+      </div>
+    </div>
   )
 }
 
