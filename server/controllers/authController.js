a69ce2f67d16287b1af84ee57d90c1b69271f8cc
Shariq Hussain <shariqhussain361@gmail.com>
2026-05-10 10:29:45 +0530

    added token for user session

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,28 +1,62 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const secretKey = process.env.JWT_SECRET_KEY;
+
 async function registerUser(req, res) {
-    let {firstName,lastName,username,password}=req.body;
-    try{
-        const duplicate = await User.findOne({username});
-        if(duplicate&&duplicate.length>0)return res.status(400).send({message:'Username already exists'});
-let user = new User({
-    firstName,
-    lastName,
-    username,
-    password
-});
-       const result = await user.save();
-       console.log(result);
-        res.status(201).send({message:'User registered successfully'});
+    let { firstName, lastName, username, password } = req.body;
+    try {
+        const duplicate = await User.findOne({ username });
+        if (duplicate && duplicate.length > 0) return res.status(400).send({ message: 'Username already exists' });
+        let user = new User({
+            firstName,
+            lastName,
+            username,
+            password
+        });
+        const result = await user.save();
+        console.log(result);
+        res.status(201).send({ message: 'User registered successfully' });
 
     }
-    catch(err){
+    catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+    }
+}
+async function loginUser(req, res) {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).send({ message: 'Invalid username or password' });
+        }
+        const isPasswordValid = await user.comparePassword(password);
+        if (!isPasswordValid) {
+            return res.status(404).send({ message: 'Wrong password' });
+        }
+        let token = jwt.sign({ userId: user?._id }, secretKey, { expiresIn: '2h' });
+        let finalData = {
+            userId: user?._id,
+            username: user?.username,
+            firstName: user?.firstName,
+            lastName: user?.lastName,
+            token
+        }
+        res.send(finalData);
+
+
+    }
+    catch (err) {
         console.log(err);
         res.status(400).send(err);
     }
 }
 
 const AuthController = {
-    registerUser
+    registerUser,
+    loginUser
 }
 
 module.exports = AuthController;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,5 +5,5 @@ const router = express.Router();
 
 
 router.post('/register',AuthController.registerUser);
-
+router.post('/login',AuthController.loginUser);
 module.exports = router;
