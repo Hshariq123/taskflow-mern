6f17d6b9103f7d2e1e6711f6225d871e6584a88a
Shariq Hussain <shariqhussain361@gmail.com>
2026-05-14 10:26:33 +0530

    added toDo routes and schema

---
+++ server/routes/ToDoRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const {createToDo} = require('../controllers/toDoController');
+const router = express.Router();
+
+
+
+router.post('/create-to-do',createToDo);
+module.exports = router;
