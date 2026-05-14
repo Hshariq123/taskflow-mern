43e29577675097b58b184b3f7e78e1bb6f3a85da
Shariq Hussain <shariqhussain361@gmail.com>
2026-05-14 12:08:25 +0530

    added middleware for authentication

---
+++ server/middleware/authJwt.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secretKey = process.env.JWT_SECRET_KEY;
+
+const authenticateToken = async (req, res, next) => {
+    let token = req.header('Authorization');
+
+    if (!token) return res.status(401).send({ message: 'Authentication Failed!' });
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) return res.status(403).send({ message: 'Token is not valid! Please Login Again.' });
+        req.user = user;
+        next();
+    })
+}
+
+
+
+module.exports = authenticateToken;
