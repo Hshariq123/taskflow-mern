eaf4a1d92798eba0d1ceb0cffa27659741424101
Shariq Hussain <shariqhussain361@gmail.com>
2026-05-16 12:12:50 +0530

    added toDo card functionality

--- client/src/pages/ToDo/ToDoList.jsx
+++ client/src/pages/ToDo/ToDoList.jsx
@@ -1,26 +1,58 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import styles from './ToDoList.module.css';
-import { Button, Divider, Input, Modal, message } from 'antd';
-import {getErrorMessage} from '../../util/GetError';
+import { Button, Divider, Input, Modal, Tag, Tooltip, message } from 'antd';
+import { getErrorMessage } from '../../util/GetError';
 import { getUserDetails } from '../../util/GetUser';
 import ToDoServices from '../../services/toDoService';
-function ToDoList() {
-  const[title,setTitle]=useState("");
-  const[description,setDescription]=useState("");
-  const [isAdding,setIsAdding]=useState(false);
-  const [loading,setLoading] = useState(false);
+import { useNavigate } from 'react-router';
+import { CheckCircleFilled, CheckCircleOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
-  const handleSubmitTask = async()=>{
+
+
+function ToDoList() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [isAdding, setIsAdding] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [allToDo, setAllToDo] = useState([]);
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    let user = getUserDetails();
+    const getAllToDo = async () => {
+      try {
+        console.log(user?.userId);
+        const response = await ToDoServices.getAllToDo(user?.userId);
+        console.log(response.data);
+        setAllToDo(response.data);
+      }
+      catch (err) {
+        console.log(err);
+        message.error(getErrorMessage(err));
+
+      }
+    }
+    if (user && user?.userId) {
+      getAllToDo();
+    }
+    else {
+      navigate('/login');
+    }
+  }, [navigate])
+
+
+  const handleSubmitTask = async () => {
     setLoading(true);
-    try{
+    try {
       const userId = getUserDetails()?.userId;
       const data = {
         title,
         description,
-        isCompleted:false,
-        userId:userId
-        
+        isCompleted: false,
+        creeatedBy: userId
+
       }
       const response = await ToDoServices.createToDo(data);
       console.log(response.data);
@@ -28,33 +60,81 @@ function ToDoList() {
       message.success("Task added.");
       setIsAdding(false);
     }
-    catch(err){
+    catch (err) {
       console.log(err);
       setLoading(false);
       message.error(getErrorMessage(err));
-      
+
     }
+  }
+
+  const getFormattedDate = (value) => {
+    let date = new Date(value);
+    let dateString = date.toDateString();
+    let hh = date.getHours();
+    let min = date.getMinutes();
+    let ss = date.getSeconds();
+    let finalDate = `${dateString} at ${hh}:${min}:${ss}`;
+    return finalDate;
+  }
+
+  const handleEdit = (item) => {
+    console.log(item);
+
+  }
+  const handleDelete = (item) => {
+    console.log(item);
+
+  }
+  const handleUpdateStatus = (id) => {
+    console.log(id);
+
   }
 
   return (
     <>
-    <Navbar active={"myTask"}/>
-    <section className={styles.toDoWrapper }>
-      <div className={styles.toDoHeader}>
-        <h2>Your Tasks</h2>
-        <Input style={{width:'50%'}}placeholder='Search Your Task Here'/>
-        <div>
-          <Button onClick={()=>setIsAdding(true)} type='primary' size='large'>Add Task</Button>
+      <Navbar active={"myTask"} />
+      <section className={styles.toDoWrapper}>
+        <div className={styles.toDoHeader}>
+          <h2>Your Tasks</h2>
+          <Input style={{ width: '50%' }} placeholder='Search Your Task Here' />
+          <div>
+            <Button onClick={() => setIsAdding(true)} type='primary' size='large'>Add Task</Button>
+          </div>
         </div>
-      </div>
-      <Divider/>
+        <Divider />
+
+        <div className = {styles.toDoListCardWrapper}>
+          {allToDo.map((item) => {
+            return (
+              <div key={item?._id} className={styles.toDoCard}>
+                <div>
+                  <div className={styles.toDoCardHeader}>
+                    <h3>{item?.title}</h3>
+                    {item?.isCompleted ? <Tag color="cyan">Completed</Tag> : <Tag color="red">Incomplete</Tag>}
+                  </div>
+                  <p>{item?.description}</p>
+                </div>
+                <div className={styles.toDoCardFooter}>
+                  <Tag>{getFormattedDate(item?.createdAt)}</Tag>
+                  <div className={styles.toDoFooterAction}>
+                    <Tooltip title="Edit Task?"><EditOutlined onClick={() => handleEdit(item)} className={styles.actionIcon} /></Tooltip>
+                    <Tooltip title="Delete Task?"><DeleteOutlined onClick={() => handleDelete(item)} style={{ color: 'red' }} className={styles.actionIcon} /></Tooltip>
+                    {item?.isCompleted ? <Tooltip title="Mark as Incomplete"><CheckCircleFilled onClick={() => handleUpdateStatus(item._id, false)} style={{ color: 'green' }} className={styles.actionIcon} /></Tooltip> : <Tooltip title="Mark as Completed"><CheckCircleOutlined onClick={() => handleUpdateStatus(item._id, true)} className={styles.actionIcon} /></Tooltip>}
+
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
 
 
-      <Modal confirmLoading={loading} title="Add New To Do Task" open={isAdding} onOk={handleSubmitTask} onCancel={()=>setIsAdding(false)}>
-        <Input style={{marginBottom:'1rem'}} placeholder='Title' value={title} onChange={(e)=>setTitle(e.target.value)} />
-        <Input.TextArea placeholder='Description' value={description} onChange={(e)=>setDescription(e.target.value)} />
-      </Modal>
-    </section>
+        <Modal confirmLoading={loading} title="Add New To Do Task" open={isAdding} onOk={handleSubmitTask} onCancel={() => setIsAdding(false)}>
+          <Input style={{ marginBottom: '1rem' }} placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} />
+          <Input.TextArea placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)} />
+        </Modal>
+      </section>
     </>
   )
 }
